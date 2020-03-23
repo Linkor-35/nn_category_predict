63d00d58adb7fbdec5958abd6f7081267c6f239c
Linkor-35 <ladrenaline@mail.ru>
2020-03-23 23:12:11 +0300

    main change

--- main.py
+++ main.py
@@ -1,44 +1,29 @@
-# from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 import pandas as pd
 
 """data sets"""
-train_courts = pd.read_csv("data.csv", delimiter=',', header=None, names=['text', 'court'])
+train_courts = pd.read_csv("data.csv", delimiter=',', header=None, names=['text', 'court']).sample(frac=1)
 test_courts = pd.read_csv("data_test.csv", delimiter=',', header=None, names=['text', 'court'])
 
 """categories"""
-court_categories = set(train_courts['court'].values)
+vec = CountVectorizer()
+train_vec = vec.fit_transform(train_courts["text"])
+test_vec = vec.transform(test_courts["text"])
 
+tdidf = TfidfTransformer()
+train_tfidf = tdidf.fit_transform(train_vec)
+test_tfidf = tdidf.fit_transform(test_vec)
 
-categories = ['alt.atheism', 'soc.religion.christian','comp.graphics', 'sci.med']
-twenty_train = fetch_20newsgroups(subset='train', categories=categories , shuffle=True, random_state=42)
+clf = RandomForestClassifier()
+clf.fit(train_tfidf, train_courts["court"].values)
+predict = clf.predict(test_tfidf)
 
-docs_new = ['God is love', 'OpenGL on the GPU is fast']
+score = clf.score(test_tfidf, test_courts["court"].values)
+print(int(score*100), "%", " predicted")
+print(predict)
+print(test_courts["court"].values)
 
-
-for t in twenty_train.target[:10]:
-    print(twenty_train.target[t],"==>", twenty_train.data[t][:30])
-
-count_vect = CountVectorizer()
-X_train_counts = count_vect.fit_transform(twenty_train.data)
-print(X_train_counts.shape)
-print(count_vect.vocabulary_.get(u'algorithm'))
-
-tfidf_transformer = TfidfTransformer()
-X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-print(X_train_tfidf.shape)
-
-clf = MultinomialNB().fit(X_train_tfidf, twenty_train.target)
-X_new_counts = count_vect.transform(docs_new)
-X_new_tfidf = tfidf_transformer.transform(X_new_counts)
-predicted = clf.predict(X_new_tfidf)
-
-for doc, category in zip(docs_new, predicted):
-    print('%r => %s' % (doc, twenty_train.target_names[category]))
-
-
-
-print()
